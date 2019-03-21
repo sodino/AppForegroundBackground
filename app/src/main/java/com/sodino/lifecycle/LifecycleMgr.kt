b706cab0c2144bea4fefda46d2cd67f4d0a89e48
cht <cht@meitu.com>
2019-03-21 14:48:14 +0800

    init

--- app/src/main/java/com/sodino/lifecycle/LifecycleMgr.kt
+++ app/src/main/java/com/sodino/lifecycle/LifecycleMgr.kt
@@ -6,35 +6,17 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
-import com.sodino.app.Test
 import java.util.*
 
 class LifecycleMgr {
+    data class Result(val change : Boolean , val size : Int)
+
     companion object {
-        val callbacks           = LinkedList<Application.ActivityLifecycleCallbacks>()
-        var activityCount       = 0
-        var resumeCount         = 0
-
-
-        open fun isTranslucentActivity(activity : Activity) : Boolean {
-//            var result = false
-//            try {
-//                val targetClassName = "com.android.internal.policy.PhoneWindow";
-//                val window = activity.window
-//                if (window.javaClass.name.equals(targetClassName)) {
-//                    val methodName = "isTranslucent"
-//                    val method = window.javaClass.getDeclaredMethod(methodName, null)
-//                    result = method.invoke(window, arrayOf<Any>()) as Boolean
-//                }
-//            } catch(t : Throwable) {
-//                t.printStackTrace()
-//            }
-//            return result
-            return Test.isTranslucentActivity(activity)
-        }
+        val callbacks                               = LinkedList<Application.ActivityLifecycleCallbacks>()
+        var activityCount                           = 0
+        var listResumeActivity                      = LinkedList<String>()
 
         val defaultCallback : Application.ActivityLifecycleCallbacks = object : Application.ActivityLifecycleCallbacks {
-            var lastResumeActivity : String = ""
 
             override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
                 Log.d("Test", "onActivityCreated -> " + activity.toString())
@@ -60,27 +42,24 @@ class LifecycleMgr {
             }
 
             override fun onActivityResumed(activity: Activity) {
-                val currentActivityString = activity.toString()
-                resumeCount ++
-
-                Log.d("Test", "onActivityResumed -> " + activity.toString() + " resumeCount=" + resumeCount)
+                val activityString = activity.toString()
+                val (change, size) = recordActivityResume(activityString)
+                Log.d("Test", "onActivityResumed -> " + activityString + " change=" + change + " resumeCount=" + size)
                 for (callback in callbacks) {
-                    if (resumeCount == 1 && callback is MyActivityLifecycleCallback) {
+                    if (change && size == 1 && callback is MyActivityLifecycleCallback) {
                         callback.onAppForeground(activity)
                     }
                     callback.onActivityResumed(activity)
                 }
+            }
 
-
-                // 处理多Activity交叉调用时未完整执行生命周期方法的纠正
-                if ((currentActivityString == lastResumeActivity  // 跳转的Activity刚onCreate或onStart还没到onResume就直接finish，则会之前的Activity会再次调用onResume
-                            && resumeCount > 1)                   // > 1表示app连接跳转。要排除掉单一Activity回桌面再回前台的场景
-                    || isTranslucentActivity(activity)            // 透明的Activity显示后，下层的Activity不会调用onStop()，过滤掉透明的Activity
-                ) {
-                    resumeCount = Math.max(1, resumeCount - 1)    // 有resume，就至少为1。避免单个TranslucentActivity桌面来回切换导致负数。
-                    Log.d("Test", "onActivityResumed -> " + activity.toString() + " correction: resumeCount=" + resumeCount)
+            private fun recordActivityResume(activityString: String) : Result {
+                var change = false
+                if (!listResumeActivity.contains(activityString)) {
+                    listResumeActivity.add(activityString)
+                    change = true;
                 }
-                lastResumeActivity = currentActivityString
+                return Result(change, listResumeActivity.size)
             }
 
             override fun onActivityPaused(activity: Activity) {
@@ -92,17 +71,22 @@ class LifecycleMgr {
             }
 
             override fun onActivityStopped(activity: Activity) {
-                resumeCount --;
-
-                Log.d("Test", "onActivityStopped -> " + activity.toString() + " resumeCount=" + resumeCount)
+                val activityString = activity.toString()
+                val (change, size) = wipeActivityResume(activityString)
+                Log.d("Test", "onActivityStopped -> " + activityString + " change=" + change + " resumeCount=" + size)
 
                 for (callback in callbacks) {
-                    if (resumeCount == 0 && callback is MyActivityLifecycleCallback) {
+                    if (change && size == 0 && callback is MyActivityLifecycleCallback) {
                         callback.onAppBackground(activity)
                     }
 
                     callback.onActivityStopped(activity)
                 }
+            }
+
+            private fun wipeActivityResume(activityString: String) : Result {
+                var change = listResumeActivity.remove(activityString)
+                return Result(change, listResumeActivity.size)
             }
 
             override fun onActivityDestroyed(activity: Activity) {
